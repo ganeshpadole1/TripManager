f973e1faf790eb543077710a130eb6645f0e3d80
ganesh padole <ganeshpadole55@gmail.com>
2020-04-02 22:39:03 +0530

    Added two classes.

---
+++ TripManager/Model/Trip/TripFunctions.swift
@@ -0,0 +1,9 @@
+//
+//  TripFunctions.swift
+//  TripManager
+//
+//  Created by ganesh padole on 02/04/20.
+//  Copyright © 2020 ganesh padole. All rights reserved.
+//
+
+import Foundation
---
+++ TripManager/Model/Trip/TripModel.swift
@@ -0,0 +1,9 @@
+//
+//  TripModel.swift
+//  TripManager
+//
+//  Created by ganesh padole on 02/04/20.
+//  Copyright © 2020 ganesh padole. All rights reserved.
+//
+
+import Foundation
