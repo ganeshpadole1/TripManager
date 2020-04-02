7404d1e6f48b704e3fa013272f42aaa6a5b84d79
ganesh padole <ganeshpadole55@gmail.com>
2020-04-03 00:02:53 +0530

    Have data populating in tableView for trips.

---
+++ TripManager/Views/TripsViewController/TripsViewController.swift
@@ -0,0 +1,46 @@
+//
+//  TripsViewController.swift
+//  TripManager
+//
+//  Created by ganesh padole on 02/04/20.
+//  Copyright © 2020 ganesh padole. All rights reserved.
+//
+
+import UIKit
+
+class TripsViewController: UIViewController {
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        tableView.dataSource = self
+        
+        TripFunctions.readTrips { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+    
+
+    
+}
+
+extension TripsViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.tripModels.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        }
+        cell?.textLabel?.text = Data.tripModels[indexPath.row].title
+        
+        return cell!
+    }
+    
+    
+}
