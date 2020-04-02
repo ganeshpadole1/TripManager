603ac79325e1b361bf1a40c3df6087926a4f96ec
ganesh padole <ganeshpadole55@gmail.com>
2020-04-02 23:12:51 +0530

    Run data retrival on background thread.

---
+++ TripManager/Model/Data.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  TripManager
+//
+//  Created by ganesh padole on 02/04/20.
+//  Copyright © 2020 ganesh padole. All rights reserved.
+//
+
+import Foundation
+
+class Data {
+    
+    static var tripModels = [TripModel]()
+}
